269dc6c933f65a882638533143e009ed63dd18a0
yyeolmu <quockkac@gmail.com>
2025-09-02 23:02:25 +0900

    Add MainScreen.js

--- front/navigation/TabNavigator.js
+++ front/navigation/TabNavigator.js
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CommunityStack from './HomeStack';
 import LogScreen from '../screens/log/logscreen';
 import PanbyeolScreen from '../screens/panbyeol/App';
+import MainScreen from '../screens/main/MainScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -9,6 +10,7 @@ export default function TabNavigator() {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
       <Tab.Screen name="AI 피싱 판별" component={PanbyeolScreen} />
+      <Tab.Screen name="홈" component={MainScreen} />
       <Tab.Screen name="로그" component={LogScreen} />
       <Tab.Screen name="커뮤니티" component={CommunityStack} />
     </Tab.Navigator>
